e989b16f89d117d106bee0a504500c10b2cdba39
rileycochrell <rileycochrell@gmail.com>
2025-11-18 11:07:22 -0700

    Update 3_change_over_years_and_comparison.py

--- pages/3_change_over_years_and_comparison.py
+++ pages/3_change_over_years_and_comparison.py
@@ -209,6 +209,7 @@ def plot_year_comparison_with_arrows(y1_values, y2_values, label1, label2, metri
         text=[f"{location1_name}<br>{label1}<br>{v:.3f}" if not pd.isna(v) else "No Data" for v in vals1],
         textposition="inside",
         insidetextanchor="middle",
+        showlegend=False
 
     ))
 
@@ -221,6 +222,7 @@ def plot_year_comparison_with_arrows(y1_values, y2_values, label1, label2, metri
         text=[f"{location1_name}<br>{label2}<br>{v:.3f}" if not pd.isna(v) else "No Data" for v in vals2],
         textposition="inside",
         insidetextanchor="middle",
+        showlegend=False
 
     ))
 
